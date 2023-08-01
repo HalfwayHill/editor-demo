85380f4cb1df3d3e7d7ad7b52fe1d9725723d655
王璞星 <wpx4lucky@163.com>
2023-08-01 13:20:23 +0800

    修复属性栏内容为空时，内容项删除

--- src/components/AttrList.vue
+++ src/components/AttrList.vue
@@ -15,7 +15,7 @@
         </el-select>
         <el-input type="number" v-else v-model="curComponent.style[key]" />
       </el-form-item>
-      <el-form-item label="内容" v-if="curComponent && curComponent.propValue && !attribute.excludes.includes(curComponent.component)">
+      <el-form-item label="内容" v-if="curComponent && !attribute.excludes.includes(curComponent.component)">
         <el-input type="textarea" v-model="curComponent.propValue" />
       </el-form-item>
     </el-form>
